9f8be2219a5b3a71ad0aa4b26f340062fc49d3e2
buserqub <sashok-59@bk.ru>
2022-10-11 22:03:44 +0500

    Edit bubble method

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ mas = []
 for i in range (n):
     mas.append(i)
     mas[i] = int(input())
-for i in range (n):
-    for j in range (n - 1):
-        if (mas[j] > mas[j + 1]):
-            mas[j], mas[j+1] = mas[j+1], mas[j]
-print (mas)
+t = input("Введите направление: по возрастанию, по убыванию\n")
+if (t == "по возрастанию"):
+    for i in range (n):
+        for j in range (n - 1):
+            if (mas[j] > mas[j + 1]):
+                mas[j], mas[j+1] = mas[j+1], mas[j]
+    print(mas)
+else:
+    if (t == "по убыванию"):
+        for i in range (n):
+            for j in range (n - 1):
+                if (mas[j] < mas[j + 1]):
+                    mas[j], mas[j+1] = mas[j+1], mas[j]
+        print (mas)
+    else:
+        print("ошибка")
